fdb0c2bc56772cd03734220ee092059b69982c4e
Tanishq Singla <tanishqsingla00@gmail.com>
2021-10-27 01:43:45 +0530

    Add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,44 @@
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import '../App.css';
+
+import { FaCalendarAlt, FaDoorOpen, FaUsers } from 'react-icons/fa'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+                <li>
+                  <Link to="/bookings" className="btn btn-header">
+                    <FaCalendarAlt />
+                    <span>Bookings</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/bookables" className="btn btn-header">
+                    <FaDoorOpen />
+                    <span>Bookables</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/users" className="btn btn-header">
+                    <FaUsers />
+                    <span>Users</span>
+                  </Link>
+                </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Switch>
+          <Route path="/bookings" element={""} />
+          <Route path="/bookables" element={""} />
+          <Route path="/users" element={""} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
